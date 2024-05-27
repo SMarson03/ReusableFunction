e875f9a133a4f48a7e7486d6fba28096733edd67
SMarson03 <167792197+SMarson03@users.noreply.github.com>
2024-05-27 08:43:05 -0400

    its super late but done

---
+++ app.js
@@ -0,0 +1,21 @@
+/**
+With that in mind, write functions that accomplish the following:
+Take an array of numbers and return the sum.
+Take an array of numbers and return the average.
+Take an array of strings and return the longest string.
+Take an array of strings, and a number and return an array 
+of the strings that are longer than the given number. 
+For example, stringsLongerThan(['say', 'hello', 'in', 'the', 'morning'], 
+3); would return ["hello", "morning"].Take a number, n, and print every 
+number between 1 and n without using loops. Use recursion.
+ */
+
+//Take an array of numbers and return the sum.
+function sumOfArray(nums){
+    let sum = 0;
+    for(let i =0; i <= nums.length; i++);
+    sum += nums[i];
+ 
+ }
+ 
+ console.log(sumOfArray([1,2,3,4,5,6,7,,8,9,10]))
